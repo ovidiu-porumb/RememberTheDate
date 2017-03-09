5c44d1af525507a0717c308158c56477a2872017
ovidiu-porumb <ovidiu.porumb@live.com>
2017-03-09 19:49:21 +0200

    Added database sync project (using DbUp)

---
+++ OP.RememberTheDate.DatabaseSync/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace OP.RememberTheDate.DatabaseSync
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //TODO: do the harlem shake here like described in http://dbup.readthedocs.io/en/latest/ 
+        }
+    }
+}
